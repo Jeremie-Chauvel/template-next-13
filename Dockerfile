29d6ea3a58f79ba7f0c551849f2c66ea75f4a2fe
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2023-02-06 18:37:40 +0100

    --wip-- [skip ci]

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+# Install dependencies only when needed
+FROM node:18.12-alpine3.17 AS pnpm-installed
+# Check https://github.com/nodejs/docker-node/tree/b4117f9333da4138b03a546ec926ef50a31506c3#nodealpine to understand why libc6-compat might be needed.
+RUN corepack enable && corepack prepare pnpm@7.9.1 --activate
+
+# Rebuild the source code only when needed
+FROM pnpm-installed AS builder
+RUN apk add --no-cache libc6-compat
+WORKDIR /app
+COPY package.json pnpm-lock.yaml ./
+RUN pnpm install --frozen-lockfile
+COPY . .
+ENV DISABLE_TYPE_CHECKING=${DISABLE_TYPE_CHECKING:-false}
+ENV DISABLE_LINTING=${DISABLE_LINTING:-false}
+RUN pnpm build
+
+# Production image, copy all the files and run next
+FROM node:18.12-alpine3.17@sha256:a136ed7b0df71082cdb171f36d640ea3b392a5c70401c642326acee767b8c540 AS runner
+RUN apk add --no-cache dumb-init
+WORKDIR /app
+
+ENV NODE_ENV production
+
+RUN addgroup -g 1001 -S nodejs
+RUN adduser -S nextjs -u 1001
+
+COPY --from=builder /app/next.config.js ./
+COPY --from=builder --chown=nextjs:nodejs /app/public ./public
+COPY --from=builder /app/package.json ./package.json
+
+# Automatically leverage output traces to reduce image size
+# https://nextjs.org/docs/advanced-features/output-file-tracing
+COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
+COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
+# handling runtime env var clientside
+COPY --chown=nextjs:nodejs .env.* ./
+# RUN chown nextjs:nodejs ./public/env
+USER nextjs
+# RUN chmod -R u+w ./public/env
+
+EXPOSE 3000
+ENV PORT 3000
+ENV APP_ENV production
+
+CMD dumb-init node server.js
--- next.config.js
+++ next.config.js
@@ -1,6 +1,68 @@
+const isTypeCheckingDisabled =
+  process.env.DISABLE_TYPE_CHECKING === 'true' ? true : false;
+
+const isEslintDisabled = process.env.DISABLE_LINTING === 'true' ? true : false;
+
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    // This header will prevent any use of user's plugins on your website
+    // If you need some permissions, change the parameter of the right permission
+    key: 'Permissions-Policy',
+    value:
+      'accelerometer=(), geolocation=(), fullscreen=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), display-capture=()',
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Automatically leverage output traces to reduce image size
+  // https://nextjs.org/docs/advanced-features/output-file-tracing
+  output: 'standalone',
+  compiler: {
+    styledComponents: true,
+  },
+  swcMinify: true,
+  typescript: { ignoreBuildErrors: isTypeCheckingDisabled },
+  eslint: { ignoreDuringBuilds: isEslintDisabled },
+  async headers() {
+    return [
+      {
+        source: '/fonts/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.DEV_ANALYZE_BUNDLE === 'true',
+});
+
+module.exports = () => withBundleAnalyzer(nextConfig);
